9588fdbacf242cbbd646554e4dea8b60128aaf6b
Jonathan <jclark81@student.gsu.edu>
2019-02-10 14:33:45 -0500

    Updates to app.js file and index.html This allows form data inserted by user to be stored in mongodb Please make sure that port number in app.js file is corresponds to your available port
    Issues:
    Although form data is posted, I am having trouble linking index.css style sheet to the index.html sheet. HTML is presented on webpage, but with no style

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var app = express();
+var port = 8080;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var mongoose = require("mongoose");
+var router = new express.Router();
+
+
+//this is a tutorial that shows me a very basic way to use Node.js and Express.js to save data to a MongoDB Databse
+//the tutorial can be found at https://codeburst.io/hitchhikers-guide-to-back-end-development-with-examples-3f97c70e0073
+
+
+//Connecting and initializing the database
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo", {
+    useNewUrlParser:true
+});
+
+//Building and defining the schema of the database
+var customerSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    age:String
+});
+
+//Create our model
+var User = mongoose.model("Customer", customerSchema);
+
+
+
+
+//express routers response is to send a file by the name of index.html
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("item saved to database");
+        })
+        .catch(err => {
+            res.status(404).send("unable to save to database");
+        });
+});
+app.listen(port, () => {
+    console.log("Server listening on port: " + port);
+});
+
+
+
+
--- connection.js
+++ connection.js
@@ -1,24 +1,91 @@
-var mongoose = require('mongoose');
-
-//ES6 Promises6
-mongoose.Promise = global.Promise;
+const mongoose = require('mongoose');
+const express = require('express');
 
 
-//Connect to the db before tests run
-before(function(done){
-
-  //Connect to mongodb
-  mongoose.connect('mongodb://localhost/reviewUDatabase', {useNewUrlParser: true});﻿
-
-  //Listen to connection once and then execute function
-  mongoose.connection.once('open',function(){
-
-    console.log('Connection has been made, now make fireworks...');
-    done();
-  //if error has been made use this function instead
-  }).on('error',function(error){
-    console.log('Connection error:', error);
-  });
+const app = express();
 
 
-});
+// development environment only - local environment
+//mongoose.connect('mongodb://localhost/TEST');
+
+
+//create a variable for mongoose connection
+//let db = mongoose.connection;
+
+//listen to the connected event
+//in the instance the database is connected, print log connected, etc.
+// db.on('connected', function() {
+//     console.log('connected to db');
+// });
+
+//listen to the event of  disconnection
+// db.on('disconnected', function() {
+//     console.log("disconnected from database");
+// });
+
+//connection error handling
+// db.on('error', function(error) {
+//     console.log("error connecting to database", error)
+// });
+
+
+// app.get('/TEST', function (req,res) {
+//    Users.find({}, function(err, TEST) {
+//        if(err) {
+//            console.log(err);
+//        } else {
+//            res.render('index', {
+//                TEST:TEST
+//            });
+//        }
+//    });
+// });
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+//var name = document.getElementById("exampleInputFirstName");
+
+MongoClient.connect(url, function(err, db) {
+    if(err) throw err;
+    var dbo  = db.db("testdb");
+    var obj = {name: "testUser", email: "test@test.com"};
+
+
+
+
+    /*Find all data within your collection*/
+
+    // dbo.collection("users").find({}).toArray(function(err, result) {
+    //     if (err) throw err;
+    //     console.log(result);
+    //     db.close();
+    // })
+
+    /* Find specified data within your collection provided */
+    //
+    // dbo.collection("users").findOne({}, function(err, result) {
+    //     if(err) throw err;
+    //     console.log(result.email);
+    //     db.close();
+    //
+    // })
+
+
+   /* Insert a document inside of that collection -- object variable must be created  */
+
+    // dbo.collection("users").insertOne(obj, function(err, res) {
+    //     if(err) throw err;
+    //     console.log("1 document inserted");
+    //     db.close();
+    // })
+
+
+  /* Create a collection   */
+
+    // dbo.createCollection("users", function(err, res) {
+    //     if (err) throw err;
+    //     console.log("Collection Created");
+    //     db.close();
+    // })
+
+})
